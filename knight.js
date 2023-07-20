6671bc37145ce84072f4103589b5d6a1d6a875ad
Ayosef <syrivera@uc.cl>
2023-07-20 16:26:33 -0400

    Add the knight move feature

--- knight.js
+++ knight.js
@@ -14,9 +14,59 @@ const gameBoardFactory = () => {
   buildGameBoard(gameBoard);
   addKnightMoves(gameBoard);
 
+  // Get the board information
   const getGameBoard = () => gameBoard;
 
-  return { getGameBoard };
+  // Return the shortest path a knight can take from the start to the end. Using BFS.
+  const knightMoves = (start, end) => {
+    // Create a queue, a visited set that stores all visited nodes and all the shortest paths from start to end.
+    const visited = new Set();
+    const queue = [];
+    const paths = [];
+
+    // Add start node to the queue.
+    queue.push({ current: start, path: [start], distance: 0 });
+
+    // Loop through the queue as long as it has elements inside.
+    while (queue.length) {
+      const { current, path, distance } = queue.shift();
+      // Mark as visited the first item on the queue.
+      visited.add(current);
+      // Check if it's reached the end point.
+      if (current === end) {
+        paths.push({ path, distance });
+      }
+      // Get the all possible moves from the current node.
+      const adjacentMoves = gameBoard.get(current);
+      // Iterate through each possible move.
+      for (let availableMove of adjacentMoves) {
+        // If the move haven't been visited, add it to the queue.
+        if (!visited.has(availableMove)) {
+          queue.push({
+            current: availableMove,
+            path: [...path, availableMove],
+            distance: distance + 1,
+          });
+        }
+      }
+    }
+
+    // Print the results to the console.
+    let count = 0;
+    console.log(
+      `=> You made it in ${paths[0]["distance"]} moves! Here's all possible paths:`
+    );
+    paths.forEach((path) => {
+      count++;
+      let currentPath = "START => ";
+      path["path"].forEach((position) => {
+        currentPath += `[${position}] - `;
+      });
+      console.log(`Path ${count}: ${currentPath} END`);
+    });
+  };
+
+  return { getGameBoard, knightMoves };
 };
 
 // Create a function that build the gameboard squares
@@ -49,14 +99,14 @@ function addKnightMoves(board) {
       let newPosX = posX + move[0];
       let newPosY = posY + move[1];
       if (0 <= newPosX && newPosX < 8 && 0 <= newPosY && newPosY < 8) {
-        let newPosition = [newPosX, newPosY];
-        board.get(square).push(newPosition);
+        board.get(square).push(`${[newPosX, newPosY]}`);
       }
     }
   }
 }
 
 const board = gameBoardFactory();
-console.log(board.getGameBoard());
+// console.log(board.getGameBoard());
+board.knightMoves("0,0", "2,7");
 
 // Make knightMoves that moves the knight from his started position to an end position in the shortest possible way.
